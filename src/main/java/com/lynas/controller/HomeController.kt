3eef7aba7740a727c32c82a5da608ef944e2d382
lynas <szlynas@gmail.com>
2017-03-13 23:56:30 +0600

    dao and simple post done

--- src/main/java/com/lynas/controller/HomeController.kt
+++ src/main/java/com/lynas/controller/HomeController.kt
@@ -1,7 +1,10 @@
 package com.lynas.controller
 
+import com.lynas.model.Person
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 
 /**
  * Created by sazzad on 7/15/16
@@ -13,5 +16,12 @@ class HomeController{
     @GetMapping(value = "/")
     fun home(): String {
         return "home"
+    }
+
+
+    @PostMapping("/addPerson")
+    fun addPerson(@ModelAttribute person: Person): String {
+        print(person)
+        return "redirect:/"
     }
 }
---
+++ src/main/java/com/lynas/model/Person.kt
@@ -0,0 +1,10 @@
+package com.lynas.model
+
+/**
+ * Created by lynas on 3/13/2017
+ */
+
+class Person {
+    var id: Int = 0
+    var name: String = ""
+}
